68ae0d37fd2412d85505ee5211e58c9a1f5d36b2
Parkpoom Pongsawat <deer@Parkpooms-MacBook-Pro.local>
2025-11-22 13:45:53 +0700

    Expo bottom tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-export default function Home() {
+export default function HomeScreen() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Home Screen</Text>
     </View>
   );
 }
---
+++ src/app/(tabs)/notifications.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function NotificationsScreen() {
+  return (
+    <View>
+      <Text>Notifications Screen</Text>
+    </View>
+  );
+}
